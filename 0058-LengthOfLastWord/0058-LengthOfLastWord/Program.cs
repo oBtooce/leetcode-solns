e0c5ea69c92c0c1ee6ccaa8293f2e28d6ad1c04c
alexwaddell <alex.b.waddell@gmail.com>
2024-01-08 18:04:33 -0500

    Solution to problem 58: Length Of Last Word

---
+++ 0058-LengthOfLastWord/0058-LengthOfLastWord/Program.cs
@@ -0,0 +1,37 @@
+﻿/* 
+* Problem 58: Length Of Last Word
+* 
+* Description: Return the length of the last word of a string.
+*              
+* Difficulty: Easy
+* Language: C#
+*/
+
+public class Solution
+{
+    public static int Main()
+    {
+        string s = "Change this string to test outputs";
+
+        int length = 0;
+
+        if (s.Length == 1)
+            return 1;
+
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            if (Char.IsLetter(s[i]))
+            {
+                length++;
+            }
+            else if (length > 0)
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine($"The length of the last word in the input string is: {length}");
+
+        return length;
+    }
+}

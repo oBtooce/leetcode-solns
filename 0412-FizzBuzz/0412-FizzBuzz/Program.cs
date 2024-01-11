9b521c8abc29e18b523fa62869a06d830612bb9e
alexwaddell <alex.b.waddell@gmail.com>
2024-01-10 20:02:56 -0500

    Solution to problem 412: Fizz Buzz

---
+++ 0412-FizzBuzz/0412-FizzBuzz/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public static void Main()
+    {
+        FizzBuzz();
+    }
+
+    public static IList<string> FizzBuzz()
+    {
+        // Use any value for testing purposes
+        int n = 30;
+
+        List<string> results = new List<string>();
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+                results.Add("FizzBuzz");
+            else if (i % 3 == 0 && i % 5 != 0)
+                results.Add("Fizz");
+            else if (i % 3 != 0 && i % 5 == 0)
+                results.Add("Buzz");
+            else
+                results.Add(i.ToString());
+        }
+
+        return results;
+    }
+}

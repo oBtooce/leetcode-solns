ba71d316a0917af729ab52303f883085cdab3f94
alexwaddell <alex.b.waddell@gmail.com>
2024-01-18 21:20:46 -0500

    Solution to problem 88: Merge Sorted Array

---
+++ 0088-MergeSortedArray/0088-MergeSortedArray/Program.cs
@@ -0,0 +1,11 @@
+﻿Merge(new int[] {1, 2, 5, 0, 0}, 3, new int[] {3, 4}, 2); // Change these values for testing
+
+void Merge(int[] nums1, int m, int[] nums2, int n)
+{
+    for (int i = m + n - 1; i >= m; i--)
+    {
+        nums1[i] = nums2[i - m];
+    }
+
+    Array.Sort(nums1);
+}

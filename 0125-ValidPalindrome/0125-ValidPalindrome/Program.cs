3a5d54763f67aa6f1587af014f027919ac13d390
alexwaddell <alex.b.waddell@gmail.com>
2024-02-25 19:25:23 -0500

    Solution to problem 125: Valid Palindrome

---
+++ 0125-ValidPalindrome/0125-ValidPalindrome/Program.cs
@@ -0,0 +1,25 @@
+﻿
+IsPalindrome("A man, a plan, a canal: Panama");
+
+bool IsPalindrome(string s)
+{
+    // Remove all non-letter and non-digit values
+    char[] strChars = s.ToCharArray();
+
+    strChars = Array.FindAll<char>(strChars, (c => char.IsLetterOrDigit(c)));
+    s = new string(strChars).ToLower();
+
+    if (s.Length == 0)
+        return true;
+
+    double strLen = Math.Floor((double)s.Length / 2);
+
+    // Check letters front and back until string is done
+    for (int i = 0; i < strLen; i++)
+    {
+        if (s[i] != s[(s.Length - 1) - i])
+            return false;
+    }
+
+    return true;
+}

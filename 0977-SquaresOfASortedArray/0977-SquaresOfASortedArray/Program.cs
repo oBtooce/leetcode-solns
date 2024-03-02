95d1ccd8e26ed875140e37c30720036e5eb1dbd9
alexwaddell <alex.b.waddell@gmail.com>
2024-03-01 21:30:18 -0500

    Solution to problem 977: Squares of a Sorted Array

---
+++ 0977-SquaresOfASortedArray/0977-SquaresOfASortedArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] nums = new int[5] { -4, -1, 0, 3, 10 };
+SortedSquares(nums);
+
+int[] SortedSquares(int[] nums)
+{
+    // Simple solution
+    for (int i = 0; i < nums.Length; i++)
+    {
+        nums[i] *= nums[i];
+    }
+
+    Array.Sort(nums);
+
+    return nums;
+}
